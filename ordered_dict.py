8382e106bdf27493609d54ca62812ef318146d8b
Mehedee Hassan <Mehedee-Hassan@users.noreply.github.com>
2021-12-31 19:23:09 +0900

    Update ordered_dict.py

--- ordered_dict.py
+++ ordered_dict.py
@@ -4,17 +4,17 @@ import collections
 
 def test():
 
-    t = [('a',1),('d',1),('c',1)]
+    t = [('a',9),('d',1),('c',1)]
 
     o_dict = collections.OrderedDict()
     for element in t:
         o_dict[element[0]] = element[1]
 
-    print(o_dict.popitem(last=False))
+    print(o_dict.popitem())
     # return the element and value
-    
-    # ouput :  ('a',1)
-    
+    # output : ('c',1)
+    # --------------------------orderd by valus not key ---------------------
+
 
 test()
 
